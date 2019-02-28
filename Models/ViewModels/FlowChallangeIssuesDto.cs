b33fd8b0cd0f1dae3ccf6d8a9bb9f596b95adeba
XTravius <bo@infoworker.no>
2019-02-28 15:35:23 +0100

    Added sample data for flow challange - persons

---
+++ Models/ViewModels/FlowChallangeIssuesDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ACDC2019SpiderpigsCovertOPs.Models.ViewModels
+{
+    public class FlowChallangeIssuesDto
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Email { get; set; }
+        public string Issue { get; set; }
+    }
+}
